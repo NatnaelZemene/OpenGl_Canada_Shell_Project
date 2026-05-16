cf94cc6df94945d8ddfa509a36bf44d639f3c4fd
NatnaelZemene <NatnaelZemene@users.noreply.github.com>
2026-05-16 14:08:42 +0300

    NatnaelZemene: Initialize project and 3D environment

---
+++ shell_logo.cpp
@@ -0,0 +1,18 @@
+﻿#include <GL/freeglut.h>
+void init() {
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+}
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Shell Logo");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
